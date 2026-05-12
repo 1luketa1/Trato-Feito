4ec75b7858edd14419be48f55dc2574777a64ab4
PuDiM6241 <pudim6241@gmail.com>
2026-05-12 21:56:04 +0000

    CRUD was created and tested

--- Services/NeoService.py
+++ Services/NeoService.py
@@ -9,47 +9,63 @@ driver = GraphDatabase.driver(
     auth=(NeoKey["USERNAME"], NeoKey["PASSWORD"])
 )
 
-#THE ACESS KEYS ARE FOR FINDING THE DATA IN OTHER DBs
+# =========================  
+#          CREATE
+# =========================
+
+#THE KEYS ARE FOR ACESSING DATA IN OTHER DBs
 def CreatePerson(name, dBAcessKey):
     query = "MERGE (p:Person {name:$name, dBAcessKey:$dBAcessKey})"
 
     with driver.session() as session:
         session.run(query, name=name, dBAcessKey=dBAcessKey)
 
-def CreateTicket(ticketId, dBAcessKey):
+def CreateTicket(value, buyerDBAcessKey, runDBAcessKey, dBAcessKey):
 
     query = """
+    MATCH (p:Person {dBAcessKey:$buyerDBAcessKey})
+
+    MATCH (r:Run {dBAcessKey:$runDBAcessKey})
+
     MERGE (t:Ticket {
-        ticketId:$ticketId,
+        value:$value,
         dBAcessKey:$dBAcessKey
     })
+
+    MERGE (p)-[:Bought]->(t)
+    MERGE (t)-[:GamblesOn]->(r)
     """
 
     with driver.session() as session:
 
         session.run(
             query,
-            ticketId=ticketId,
+            value=value,
+            buyerDBAcessKey=buyerDBAcessKey,
+            runDBAcessKey=runDBAcessKey,
             dBAcessKey=dBAcessKey
         )
 
 
-def CreateDeposit(depositId, value, dBAcessKey):
+def CreateDeposit(value, buyerdBAcessKey, dBAcessKey):
 
     query = """
+    MATCH (p:Person {dBAcessKey:$buyerdBAcessKey})
+
     MERGE (d:Deposit {
-        depositId:$depositId,
         dBAcessKey:$dBAcessKey
     })
     SET d.value = $value
+
+    MERGE (p)-[:Deposited]->(t)
     """
 
     with driver.session() as session:
 
         session.run(
             query,
-            depositId=depositId,
             value=value,
+            buyerdBAcessKey=buyerdBAcessKey,
             dBAcessKey=dBAcessKey
         )
 
@@ -90,21 +106,536 @@ def CreateTrack(name, dBAcessKey):
         )
 
 
-def CreateRun(runId, date, dBAcessKey):
+#ESSE DAQUI E PERIGOSO TEM QUE TESTA SEPARADO
+def CreateRun(date, horsesDBAcessKeys, trackDBAcessKey, dBAcessKey):
 
     query = """
     MERGE (r:Run {
-        runId:$runId,
         dBAcessKey:$dBAcessKey
     })
+
     SET r.date = $date
+
+    MATCH (t:Track {
+        dBAcessKey:$trackDBAcessKey
+    })
+
+    MERGE (r)-[:OccursAt]->(t)
+
+    WITH r
+    UNWIND $horsesDBAcessKeys AS horseKey
+
+    MATCH (h:Horse {
+        dBAcessKey:horseKey
+    })
+
+    MERGE (h)-[:RacesIn]->(r)  
     """
 
     with driver.session() as session:
 
         session.run(
             query,
-            runId=runId,
             date=date,
+            horsesDBAcessKeys=horsesDBAcessKeys,
+            trackDBAcessKey=trackDBAcessKey,
             dBAcessKey=dBAcessKey
         )
+
+# =========================
+#        RELATIONS
+# ========================= 
+
+def CreateViewedHorseRelation(personDBAcessKey, horseDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (h:Horse {
+        dBAcessKey:$horseDBAcessKey
+    })
+
+    MERGE (p)-[:Viewed]->(h)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            horseDBAcessKey=horseDBAcessKey
+        )
+
+
+def CreateFavoriteHorseRelation(personDBAcessKey, horseDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (h:Horse {
+        dBAcessKey:$horseDBAcessKey
+    })
+
+    MERGE (p)-[:Favorited]->(h)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            horseDBAcessKey=horseDBAcessKey
+        )
+
+
+def CreateViewedRunRelation(personDBAcessKey, runDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (r:Run {
+        dBAcessKey:$runDBAcessKey
+    })
+
+    MERGE (p)-[:Viewed]->(r)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            runDBAcessKey=runDBAcessKey
+        )
+
+
+def CreateFavoriteRunRelation(personDBAcessKey, runDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (r:Run {
+        dBAcessKey:$runDBAcessKey
+    })
+
+    MERGE (p)-[:Favorited]->(r)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            runDBAcessKey=runDBAcessKey
+        )
+
+
+def CreateViewedTrackRelation(personDBAcessKey, trackDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (t:Track {
+        dBAcessKey:$trackDBAcessKey
+    })
+
+    MERGE (p)-[:Viewed]->(t)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            trackDBAcessKey=trackDBAcessKey
+        )
+
+
+def CreateFavoriteTrackRelation(personDBAcessKey, trackDBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$personDBAcessKey
+    })
+
+    MATCH (t:Track {
+        dBAcessKey:$trackDBAcessKey
+    })
+
+    MERGE (p)-[:Favorited]->(t)
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            personDBAcessKey=personDBAcessKey,
+            trackDBAcessKey=trackDBAcessKey
+        )
+
+# =========================     
+#          UPDATE
+# =========================
+
+def UpdatePersonName(dBAcessKey, newName):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET p.name = $newName
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newName=newName
+        )
+
+
+def UpdateHorseName(dBAcessKey, newName):
+
+    query = """
+    MATCH (h:Horse {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET h.name = $newName
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newName=newName
+        )
+
+
+def UpdateTrackName(dBAcessKey, newName):
+
+    query = """
+    MATCH (t:Track {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET t.name = $newName
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newName=newName
+        )
+
+
+def UpdateTicketValue(dBAcessKey, newValue):
+
+    query = """
+    MATCH (t:Ticket {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET t.value = $newValue
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newValue=newValue
+        )
+
+
+def UpdateDepositValue(dBAcessKey, newValue):
+
+    query = """
+    MATCH (d:Deposit {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET d.value = $newValue
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newValue=newValue
+        )
+
+
+def UpdateRunDate(dBAcessKey, newDate):
+
+    query = """
+    MATCH (r:Run {
+        dBAcessKey:$dBAcessKey
+    })
+
+    SET r.date = $newDate
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey,
+            newDate=newDate
+        )
+
+
+# =========================
+#          DELETE
+# =========================
+
+def DeletePerson(dBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE p
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+
+def DeleteHorse(dBAcessKey):
+
+    query = """
+    MATCH (h:Horse {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE h
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+
+def DeleteTrack(dBAcessKey):
+
+    query = """
+    MATCH (t:Track {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE t
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+
+def DeleteTicket(dBAcessKey):
+
+    query = """
+    MATCH (t:Ticket {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE t
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+
+def DeleteDeposit(dBAcessKey):
+
+    query = """
+    MATCH (d:Deposit {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE d
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+
+def DeleteRun(dBAcessKey):
+
+    query = """
+    MATCH (r:Run {
+        dBAcessKey:$dBAcessKey
+    })
+
+    DETACH DELETE r
+    """
+
+    with driver.session() as session:
+
+        session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+
+# =========================
+#           READ
+# =========================
+
+def ReadPerson(dBAcessKey):
+
+    query = """
+    MATCH (p:Person {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN p
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["p"])
+
+
+def ReadHorse(dBAcessKey):
+
+    query = """
+    MATCH (h:Horse {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN h
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["h"])
+
+
+def ReadTrack(dBAcessKey):
+
+    query = """
+    MATCH (t:Track {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN t
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["t"])
+
+
+def ReadTicket(dBAcessKey):
+
+    query = """
+    MATCH (t:Ticket {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN t
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["t"])
+
+
+def ReadDeposit(dBAcessKey):
+
+    query = """
+    MATCH (d:Deposit {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN d
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["d"])
+
+
+def ReadRun(dBAcessKey):
+
+    query = """
+    MATCH (r:Run {
+        dBAcessKey:$dBAcessKey
+    })
+
+    RETURN r
+    """
+
+    with driver.session() as session:
+
+        result = session.run(
+            query,
+            dBAcessKey=dBAcessKey
+        )
+        data = result.single()
+        return dict(data["r"])
+
+
+# =========================
+#          Filter  
+# ========================= 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import Services.NeoService as neo
+from datetime import datetime
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,27 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"msg": "API funcionando"}
+
+'''
+neo.CreatePerson("Carlinhos, O ultimo domador de cavalos", "TestPer1")
+neo.CreateTrack("Subida Descida Subida Descida", "TestTra1")
+neo.CreateHorse("GABRIEL, O INDOMAVEL", "TestHor1")
+neo.CreateHorse("CAVALO COM TATUAGEM DO LINKINPARK", "TestHor2")
+neo.CreateDeposit(2050.00, "TestPer1", "TestDep1")
+neo.CreateRun(datetime.now(), ["TestHor2", "TestHor1"], "TestTra1", "TestRun1")
+neo.CreateViewedRunRelation("TestPer1", "TestRun1")
+neo.CreateFavoriteRunRelation("TestPer1", "TestRun1")
+neo.CreateViewedHorseRelation("TestPer1", "TestHor1")
+neo.CreateFavoriteHorseRelation("TestPer1", "TestHor1")
+neo.CreateViewedTrackRelation("TestPer1", "TestTra1")
+neo.CreateFavoriteTrackRelation("TestPer1", "TestTra1")
+neo.CreateTicket(2050.00, "TestPer1", "TestRun1", "TestTic1")
+
+print(neo.ReadPerson("TestPer1"))
+print(neo.ReadTrack("TestTra1"))
+print(neo.ReadHorse("TestHor1"))
+print(neo.ReadHorse("TestHor2"))
+print(neo.ReadDeposit("TestDep1"))
+print(neo.ReadRun("TestRun1"))
+print(neo.ReadTicket("TestTic1"))'''
+
